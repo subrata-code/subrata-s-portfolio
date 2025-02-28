6c8e83fb53b14c18f9c096961d0d7a691065eefc
subrata-code <bagsubrata193@gmail.com>
2025-02-28 17:57:21 +0530

    add devs.png

---
+++ script.js
@@ -0,0 +1,9 @@
+// Typed.js Animation
+var typed = new Typed("#typed-text", {
+    strings: ["Web Devoloper", "Python devoloper", "Tech Enthusiast"],
+    typeSpeed: 100,  // Speed of typing
+    backSpeed: 50,   // Speed of deleting text
+    loop: true,      // Infinite loop
+    showCursor: true,
+    cursorChar: "|",
+});
