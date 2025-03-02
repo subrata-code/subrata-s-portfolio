e0963145619ef706118e653529b22eba090f8c95
subrata-code <bagsubrata193@gmail.com>
2025-03-02 16:32:22 +0530

    Update script.js
    responsive

--- script.js
+++ script.js
@@ -35,3 +35,27 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+document.addEventListener("DOMContentLoaded", function () {
+    const burger = document.querySelector(".burger-menu");
+    const nav = document.querySelector("nav");
+
+    burger.addEventListener("click", function () {
+        nav.classList.toggle("active");
+    });
+});
+document.addEventListener("DOMContentLoaded", function () {
+    const skillSection = document.querySelector(".secondsection");
+
+    function checkScroll() {
+        const sectionPosition = skillSection.getBoundingClientRect().top;
+        const screenPosition = window.innerHeight / 1.3;
+
+        if (sectionPosition < screenPosition) {
+            skillSection.classList.add("visible");
+        }
+    }
+
+    window.addEventListener("scroll", checkScroll);
+});
+  
+
